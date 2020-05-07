d7ab469a4652760f89f629d7cdb951d9db388992
pat-white-code <pat.k.white@gmail.com>
2020-05-07 15:15:14 -0500

    adds nav, npm installs material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Router />
     </BrowserRouter>
   );
