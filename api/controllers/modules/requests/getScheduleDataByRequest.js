32f0ecf824c0a7e6fed8f05b3d587e063a414ff9
pat-white-code <pat.k.white@gmail.com>
2020-05-25 10:56:57 -0500

    fetches zip code from request db and sends to getScheduleDataByRequest.

--- api/controllers/modules/requests/getScheduleDataByRequest.js
+++ api/controllers/modules/requests/getScheduleDataByRequest.js
@@ -5,7 +5,11 @@ const getScheduleDataByRequest = (req, res, next) => {
   // req.body: {requestId: 1}
   const {requestId, instrumentId, studentAge} = req.body;
   const fetchLessonData = async () => {
-    let response = await axios.get(`http://127.0.0.1:3000/api/schedules/request/${requestId}?instrumentId=${instrumentId}&zipCode=78746&studentAge=${studentAge}`);
+    // instrumentId, studentAge
+    let zipRes = await axios.get(`http://127.0.0.1:3000/api/addresses/request/${requestId}`);
+    let zipCode = zipRes.data[0].zip_code;
+    console.log('ZIP CODE:    ', zipCode);
+    let response = await axios.get(`http://127.0.0.1:3000/api/schedules/request/${requestId}?instrumentId=${instrumentId}&zipCode=${zipCode}&studentAge=${studentAge}`);
     // let json = await response.json();
     // console.log('json', json)
     // console.log(response)
