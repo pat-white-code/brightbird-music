d4f23609423b09597e6443bdaf3879123eab1f96
pat-white-code <pat.k.white@gmail.com>
2020-05-30 15:56:36 -0500

    puts addRequest inside modal

--- src/components/ViewRequests.js
+++ src/components/ViewRequests.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import RequestCard from './RequestCard';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import AddRequestModal from './AddRequestModal';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -29,6 +30,7 @@ const ViewRequests = (props) => {
           <IconButton aria-label="delete" className={classes.margin}>
             <AddCircleIcon fontSize="large" />
           </IconButton>
+          <AddRequestModal />
         </>
 
       )
