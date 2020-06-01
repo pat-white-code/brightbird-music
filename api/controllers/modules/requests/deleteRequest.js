dad9d84b57e73790f202acfcd4f464d1994ab71f
pat-white-code <pat.k.white@gmail.com>
2020-06-01 10:44:22 -0500

    edits deleteRequest module so that it sets is_active = 0

--- api/controllers/modules/requests/deleteRequest.js
+++ api/controllers/modules/requests/deleteRequest.js
@@ -3,16 +3,15 @@ const mysql = require('mysql');
 
 const deleteRequest = (req, res) => {
   let sql = `
-    DELETE FROM service_requests
-    WHERE id = ?
-  `
+    UPDATE service_requests
+    SET is_active = 0
+    WHERE id = ?;
+    `;
   let replacements = [req.params.requestId]
-
   sql = mysql.format(sql, replacements);
-
   pool.query(sql, (err, results)=>{
     if(err) {return res.status(500).send(err)}
-    return res.status(204).send('content deleted successfully')
+    return res.status(200).send('is_active set to false')
   })
 }
 
